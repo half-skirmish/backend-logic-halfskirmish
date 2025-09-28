5b84ed489c76e9c5e5dff4c79b681da1b534b740
naman-1905 <namanoj19@gmail.com>
2025-09-28 12:12:31 +0000

    Updated the User model to store user info under the blog_authors collection in Mongo DB

--- models/User.js
+++ models/User.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
     name: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }, // hashed password
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema, "blog_authors");
